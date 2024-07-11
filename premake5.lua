36f789b4dd79bc4b7e34eebd73ff0b9e875c905c
Gary <qiuhuidi31@Gmail.com>
2024-07-11 18:08:24 +0800

    premake5.lua & .gitignore

--- premake5.lua
+++ premake5.lua
@@ -10,17 +10,27 @@ project "yaml-cpp"
     {
         "src/**.h",
         "src/**.cpp",
-        "include/yaml-cpp/**.h"
+        "include/**.h"
+    }
+
+    includedirs
+    {
+        "include"
+    }
+
+    defines
+    {
+        "YAML_CPP_STATIC_DEFINE",
     }
 
     filter "system:windows"
         systemversion "latest"
-        cppdialect "C++17"
+        cppdialect "C++20"
 
     filter "system:linux"
         pic "On"
         systemversion "latest"
-        cppdialect "C++17"
+        cppdialect "C++20"
 
     filter "configurations:Debug"
         runtime "Debug"
